437d8c1d04f3b1fb25ed51d812a978f74d20d199
Giramahirweurbain <131149717+Giramahirweurbain@users.noreply.github.com>
2024-04-02 12:03:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const wrapper=document.queryselector('.wrapper');
+const login=document.queryselector ('.login-link');
+const registerlink=document.queryselector('.register-link');
+
+const registerlink=document.queryselector('.register-link');
+const btnpoup=document.queryselector('.register-link');
+
+registerlink.addEventListener('click',()=>{
+	wrapper.classlist.add('active');
+});
+loginlink.addEventListener('click',()=>{
+	.wrapper.classlist.add('active');
+})
+btnpoup.addEventListener('click',()=>{
+	.wrapper.classlist.add('active-poup');
+})
